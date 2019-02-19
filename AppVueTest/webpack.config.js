551f7a8a305bae71fcda42dbe2844fbca9e2c644
a.sazhin <a.sazhin@itway.company>
2019-02-19 13:17:19 +0300

    у

---
+++ AppVueTest/webpack.config.js
@@ -0,0 +1,78 @@
+﻿/// <binding />
+"use strict";
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: "./wwwroot/js/site.js",
+    output: {
+        filename: './wwwroot/js/dist/bundle.js',
+        path: __dirname
+        //path: path.join(__dirname+"/wwwroot/js", ""),
+        //path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'source-map',
+    //devServer: {
+    //    contentBase: ".",
+    //    host: "localhost",
+    //    port: 9000
+    //},
+
+
+
+    module: {
+        rules: [
+            // {
+            //   test: /\.css$/,
+            //   use: [
+            //     'vue-style-loader',
+            //     {
+            //       loader: 'css-loader',
+            //       options: {
+            //         // enable CSS Modules
+            //         modules: true,
+            //         // customize generated class names
+            //         localIdentName: '[local]_[hash:base64:8]'
+            //       }
+            //     }
+            //   ]
+            // },
+            {
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // это будет применяться к файлам `.js`
+            // А ТАКЖЕ к секциям `<script>` внутри файлов `.vue`
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // это будет применяться к файлам `.css`
+            // А ТАКЖЕ к секциям `<style>` внутри файлов `.vue`
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+
+    },
+    plugins: [
+        // убедитесь что подключили плагин!
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: {
+            'vue$': './wwwroot/lib/vue/vue.js'
+            //'vue$': 'C:/SAS/Projects/Vue/scripts/vue.js' // 'vue/dist/vue.common.js' for webpack 1
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    }
+};
---
+++ AppVueTest/wwwroot/js/site.js
@@ -0,0 +1,60 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//alert(6);
+
+
+// Init F7 Vue Plugin
+Framework7.use(Framework7Vue);
+
+
+
+// Init Page Components
+Vue.component('page-about', {
+    template: '#page-about'
+});
+Vue.component('page-form', {
+    template: '#page-form'
+});
+Vue.component('page-dynamic-routing', {
+    template: '#page-dynamic-routing'
+});
+Vue.component('page-not-found', {
+    template: '#page-not-found'
+});
+
+// Init App
+new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            // Framework7 parameters here
+            f7params: {
+                root: '#app', // App root element
+                id: 'io.framework7.testapp', // App bundle ID
+                name: 'Framework7', // App name
+                theme: 'auto', // Automatic theme detection
+                // App routes
+                routes: [
+                    {
+                        path: '/about/',
+                        component: 'page-about'
+                    },
+                    {
+                        path: '/form/',
+                        component: 'page-form'
+                    },
+                    {
+                        path: '/dynamic-route/blog/:blogId/post/:postId/',
+                        component: 'page-dynamic-routing'
+                    },
+                    {
+                        path: '(.*)',
+                        component: 'page-not-found',
+                    },
+                ],
+            }
+        }
+    },
+});
